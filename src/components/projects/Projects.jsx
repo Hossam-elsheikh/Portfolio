2f69c9182b20d34b203193ef91a2d1bfc543677a
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-10-20 02:03:13 +0300

    edit

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import BgContainer from "../UI/bgCont/BgContainer";
 import classes from "./projects.module.css";
 import { Link } from "react-router-dom";
-import noter from '../../assets/noter.png'
 import { useSelector } from "react-redux";
 const Projects = () => {
   const projects = useSelector((data)=> data.projects.projects)
